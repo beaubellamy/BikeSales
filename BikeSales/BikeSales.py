925dbd5de1ddfd631b2cb51de8bb15723de95839
beaubellamy <beau.bellamy@hotmail.com>
2019-01-27 19:34:25 +1100

    Updated code to read all the details of the bike. Created seperate functions to extract the detail names and values to be inserted into the dictionary.

--- BikeSales/BikeSales.py
+++ BikeSales/BikeSales.py
@@ -7,49 +7,104 @@ import selenium.common.exceptions as seleniumException
 
 import configdata
 
-chromedriver = configdata.chromedriver
-baseUrl = "https://www.bikesales.com.au/"
+def get_Element_Names(element):
+   return element.find_elements_by_tag_name('th')
 
-sortedBikes = "https://www.bikesales.com.au/bikes/?q=Service.Bikesales.&Sort=Price"
-bikesPerPage = 12
-numberOfBikes = 100 # default value
+def get_Element_Name(element):
+   return element.find_element_by_tag_name('th')
 
-driver = webdriver.Chrome(chromedriver)
-driver.get(sortedBikes)
+def get_Element_Values(element):
+    return element.find_elements_by_tag_name('td')
 
-# Add filters
-# This will be left until last to deal with
-
-# Figour out how many pages there will be.
-elements = driver.find_elements_by_class_name('title')
-for element in elements:
-    if ("Motorcycles for sale" in element.text):
-        tokens = element.split()
-        for token in tokens:
-            token = token.replace(',','')
-            if (token.isdigit()):
-                numberOfBikes = int(token)
+def get_Element_Value(element):
+    return element.find_element_by_tag_name('td')
+    
 
 
-numberOfPages = numberOfBikes / bikesPerPage
+if __name__ == '__main__':
 
-# Get a link to all the pages
+    chromedriver = configdata.chromedriver
+    #baseUrl = "https://www.bikesales.com.au/"
+
+    sortedBikes = "https://www.bikesales.com.au/bikes/?q=Service.Bikesales.&Sort=Price"
+    bikesPerPage = 12 # assumed constant
+    numberOfBikes = 100 # default value
+    datadict = {}
+
+    driver = webdriver.Chrome(chromedriver)
+    driver.get(sortedBikes)
+
+    # Add filters
+    # This will be left until last to deal with
+
+    # Figour out how many pages there will be.
+    elements = driver.find_elements_by_class_name('title')
+    for element in elements:
+        if ("Motorcycles for sale" in element.text):
+            tokens = element.split()
+            for token in tokens:
+                token = token.replace(',','')
+                if (token.isdigit()):
+                    numberOfBikes = int(token)
 
 
-# Get the list of all the bikes on the page
-bikesInPage = driver.find_elements_by_css_selector('.listing-item.standard')
-# Extract the links into a list
-#bikeLinks = 
+    numberOfPages = numberOfBikes / bikesPerPage
+
+    # Get a link to all the pages
 
 
-# Go to each bike link
-for bikes in bikeLinks:
+    # Get the list of all the bikes on the page
+    bikesInPage = driver.find_elements_by_css_selector('.listing-item.standard')
 
-    driver.get(bike)
+    bikeLinks = []
+    # Extract the links into a list
+    for bike in bikesInPage:
+        link = bike.find_element_by_css_selector('a').get_attribute('href')
+        bikeLinks.append(link)
 
-    # save details to a data frame
+
+    # Go to each bike link
+    for bike in bikeLinks:
+        attempt = 0
+    
+        sleep(5)
+        print ('URL: ',bike)
+        driver.get(bike)
+        # Wait for completion
+
+        # check access is allowed
+        while (attempt < 3 and driver.find_element_by_tag_name('h1').text == 'Access Denied'):
+            sleep(5)
+            driver.get(bike)
+            print (attempt)
+            attempt += 1
+
+        if (driver.find_element_by_tag_name('h1').text == 'Access Denied'):
+            continue
+
+        sleep(5)
+
+        # save details to a data frame
+        
+        # Bike Details section
+        details = driver.find_element_by_css_selector('section.component:nth-child(2)')
+        detailsName = get_Element_Names(details)
+        detailsValue = get_Element_Values(details)
+
+       
+        # how do we deal with new column values ????
+        for idx in range(len(detailsName)):
+            key = detailsName[idx].text
+            value = detailsValue[idx].text
+            if key in list(datadict.keys()):
+                datadict[key].append(value)
+            else:
+                datadict[key] = [value]
+
+        
+
 
     # Go back to the previous page to access the next bike link
-    
+    print(datadict.shape)
 
 
